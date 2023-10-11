b03490c405394ca306e8e60e98b6a5094feedfc9
wonho an <wonho.an@monstar-lab.com>
2023-10-11 10:17:45 +0900

    feat: dayjs timezone

--- functions/src/utils/date.ts
+++ functions/src/utils/date.ts
@@ -1,19 +1,21 @@
 import * as dayjs from "dayjs";
+import * as utc from "dayjs/plugin/utc";
 import * as timezone from "dayjs/plugin/timezone";
 
 const tz = "Asia/Tokyo";
 
+dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault(tz);
 
 export const DATE_FORMAT = "YYYY-MM-DD";
 export const DATETIME_FORMAT = "YYYY/MM/DD HH:mm";
+export const day = () => dayjs().tz();
 
 // api
-export const currentDatetime = () => dayjs().add(9, "hours").format(DATETIME_FORMAT);
+export const currentDatetime = () => day().format(DATETIME_FORMAT);
 
 // batch
-export const day = () => dayjs().tz();
-export const currentTimestamp = () => dayjs().tz().format();
-export const currentLocalTimestamp = () => dayjs().tz().format();
-export const currentDate = () => dayjs().tz().format(DATE_FORMAT);
+export const currentTimestamp = () => day().format();
+export const currentLocalTimestamp = () => day().format();
+export const currentDate = () => day().format(DATE_FORMAT);
